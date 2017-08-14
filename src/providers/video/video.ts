299dcadad4d975c10023146e265754ce7afb6e60
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2017-08-14 23:18:54 +0200

    New function to get video thumbnails

--- src/providers/video/video.ts
+++ src/providers/video/video.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { VideoModel } from '../../app/models/video.model';
 import { ConfigProvider } from '../../providers/config/config';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class VideoProvider {
@@ -13,5 +14,15 @@ export class VideoProvider {
   list(): Observable<Array<VideoModel>> {
     return this.http.get(`${this.config.apiUrl}/video`)
       .map(res => res.json());
+  }
+
+  thumbnails(videoId: Number): Array<String> {
+    const thumbnails = [];
+
+    for (let i = 1; i <= 4; i++) {
+      thumbnails.push(`${this.config.apiUrl}/video/${videoId}/thumbnail/${i}`);
+    }
+
+    return thumbnails;
   }
 }
