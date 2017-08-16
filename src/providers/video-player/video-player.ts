43ae623ec8f17556caa5ccddac33bea251e38299
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2017-08-16 22:33:08 +0200

    Add callbacks has parameters

--- src/providers/video-player/video-player.ts
+++ src/providers/video-player/video-player.ts
@@ -6,7 +6,6 @@ import { AuthProvider } from '../auth/auth';
 
 @Injectable()
 export class VideoPlayerProvider {
-  options: StreamingVideoOptions;
   orientation = 'landscape';
   user: UserModel;
 
@@ -14,24 +13,15 @@ export class VideoPlayerProvider {
     private streamingMedia: StreamingMedia, 
     private config: ConfigProvider, 
     private auth: AuthProvider) {
-    this.options = {
-      successCallback: this.success,
-      errorCallback: this.error,
-      orientation: this.orientation
-    }  
     this.auth.userEvent.subscribe(user => this.user = user);
   }
 
-  play(videoId) {
-    console.log(`${this.config.apiUrl}/video/${videoId}/t/${this.user.token}`);
-    this.streamingMedia.playVideo(`${this.config.apiUrl}/video/${videoId}/t/${this.user.token}`, this.options);
-  }
-
-  success() {
-    console.log('Video played');
-  }
-
-  error(e) {
-    console.log('Error streaming');
+  play(videoId, successCallback, errorCallback) {
+    console.log(`${this.config.videoUrl}/video/${videoId}/t/${this.user.token}`);
+    this.streamingMedia.playVideo(`${this.config.videoUrl}/video/${videoId}/t/${this.user.token}`, {
+      successCallback,
+      errorCallback,
+      orientation: this.orientation
+    });    
   }
 }
