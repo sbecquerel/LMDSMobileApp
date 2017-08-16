0cd52c204c92d017071faeabcc21c94bd32f08f6
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2017-08-16 22:32:15 +0200

    Transform list cards to simple list

--- src/pages/videos/videos.ts
+++ src/pages/videos/videos.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { VideoProvider } from '../../providers/video/video';
-import { VideoPlayerProvider} from '../../providers/video-player/video-player';
+//import { VideoPlayerProvider} from '../../providers/video-player/video-player';
 import { VideoModel } from '../../app/models/video.model';
 import { UserModel } from '../../app/models/user.model';
 import { AuthProvider } from '../../providers/auth/auth';
+import { VideoPage } from '../video/video';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-videos',
@@ -14,6 +16,7 @@ export class VideosPage {
 
   videos: Array<VideoModel> = [];
   private _searchQuery = '';
+  private userEventsSubscription: Subscription;
 
   set searchQuery(value: string) {
     this._searchQuery = value.toLowerCase().trim();
@@ -27,17 +30,28 @@ export class VideosPage {
     public navCtrl: NavController, 
     public navParams: NavParams, 
     private videoProvider: VideoProvider,
-    private player: VideoPlayerProvider,
     private auth: AuthProvider) {
 
-    this.auth.userEvent.subscribe((user: UserModel) => {
+    
+  }
+
+  ngOnInit() { 
+    this.userEventsSubscription  = this.auth.userEvent.subscribe((user: UserModel) => {
+      console.log('userEvent.subscribe');
       if (user !== undefined) {
         this._loadVideos();
       }
-    })
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.userEventsSubscription) {
+      this.userEventsSubscription.unsubscribe();
+    }
   }
 
   _loadVideos() {
+    console.log('_loadVideos');
     this.videoProvider.list()
       .subscribe(videos => this.videos = videos, err => {
         if (err.status === 403) {
@@ -46,26 +60,8 @@ export class VideosPage {
       });
   }
 
-  playVideo(videoId) {
-    this.player.play(videoId);
-  }
-
-  getTags(tags) {
-    if (!tags) {
-      return [];
-    }
-    return tags.split(' ').map(tag => tag.trim())
-  }
-
-  getFavorite(favorite) {
-    if (favorite === null || favorite === undefined || favorite == 0) {
-      return false;
-    }
-
-    return true;
-  }
-
   getVideos() {
+    console.log('getVideos');
     this.videoProvider.list()
       .subscribe(videos => this.videos = videos.filter((video: VideoModel) => {
         return video.title.concat(video.tags).toLowerCase().indexOf(this.searchQuery) > -1;
@@ -76,15 +72,12 @@ export class VideosPage {
       });
   }
 
-  getThumbnails(videoId) {
-    return this.videoProvider.thumbnails(videoId);
-  }
-
   resetList() {
     this._loadVideos();
   }
 
   doRefresh(refresher) {
+    console.log('doRefresh');
     this.videoProvider.list()
       .subscribe(videos => {
         this.videos = videos;
@@ -95,5 +88,9 @@ export class VideosPage {
         }
         refresher.complete();
       });
+  }
+
+  goToVideo(video) {
+    this.navCtrl.push(VideoPage, { video });
   }
 }
