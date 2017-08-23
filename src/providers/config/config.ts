b6abe8eea2d9441ed5fca5a3654659736e0c04a2
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2017-08-23 22:54:12 +0200

    Remove device test

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Platform } from 'ionic-angular';
 
 @Injectable()
 export class ConfigProvider {
 
-  //_apiUrl = 'http://127.0.0.1:8080';
-  _apiUrl = 'http://91.121.152.202:8080';
-
-  get apiUrl() {
-    return this._apiUrl;
-  }
-
+  // apiUrl = 'http://91.121.152.202:8080';
+  apiUrl = '/api';
   videoUrl = 'http://91.121.152.202:8080';
 
-  constructor(private platform: Platform) {
-    if (this.platform.is('core') || this.platform.is('mobileweb')) {
-      this._apiUrl = '/api';
-    }
+  constructor() {
   }
 }
